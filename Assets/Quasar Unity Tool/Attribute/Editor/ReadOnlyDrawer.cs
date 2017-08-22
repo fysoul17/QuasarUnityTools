70f2171a23039d6503459b7dc86f4bc7b6723748
Terry K <terry@gixgames.com>
2017-08-22 16:06:58 +0900

    ReadOnly Attribute import
    ReadOnly Attribute import

---
+++ Assets/Quasar Unity Tool/Attribute/Editor/ReadOnlyDrawer.cs
@@ -0,0 +1,30 @@
+﻿////////////////////////////////////////////////////////////////////////////
+// ReadOnly Property Attribute
+//
+// Author: Terry Keetae Kwon
+// 
+// Copyright (C) 2015 Terry K. @ GIX Entertainment Inc. & Quasar Inc.
+//
+////////////////////////////////////////////////////////////////////////////
+
+using UnityEditor;
+using UnityEngine;
+
+namespace Quasar.Attribute
+{
+    [CustomPropertyDrawer(typeof(ReadOnlyPropertyAttribute))]
+    public class ReadOnlyDrawer : PropertyDrawer
+    {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(property, label, true);
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            GUI.enabled = false;
+            EditorGUI.PropertyField(position, property, label, true);
+            GUI.enabled = true;
+        }
+    }
+}
---
+++ Assets/Quasar Unity Tool/Attribute/ReadOnlyPropertyAttribute.cs
@@ -0,0 +1,18 @@
+﻿////////////////////////////////////////////////////////////////////////////
+// ReadOnly Property Attribute
+//
+// Author: Terry Keetae Kwon
+// 
+// Copyright (C) 2015 Terry K. @ GIX Entertainment Inc. & Quasar Inc.
+//
+////////////////////////////////////////////////////////////////////////////
+
+using UnityEngine;
+
+namespace Quasar.Attribute
+{
+    public class ReadOnlyPropertyAttribute : PropertyAttribute
+    {
+
+    }
+}
