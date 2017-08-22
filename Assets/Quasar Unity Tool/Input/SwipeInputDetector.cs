4a0e7ae40c0abb1d4b349d95bb0182e024f6421b
Terry K <terry@gixgames.com>
2017-08-22 15:52:10 +0900

    Swipe input detector import
    Swipe input detector import

---
+++ Assets/Quasar Unity Tool/Input/SwipeInputDetector.cs
@@ -0,0 +1,58 @@
+﻿////////////////////////////////////////////////////////////////////////////
+// Swipe Input Detector
+//
+// Author: Terry Keetae Kwon
+// 
+// Copyright (C) 2015 Terry K. @ GIX Entertainment Inc. & Quasar Inc.
+//
+////////////////////////////////////////////////////////////////////////////
+
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Quasar.Input
+{
+    public class SwipeInputDetector : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+    {
+        public static Action<InputDirection> InputDerectionCallback;
+        public float clickThreshold = 50f;
+
+        Vector2 startPoint;
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            startPoint = eventData.pointerCurrentRaycast.screenPosition;
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            Vector2 deltaPoint = eventData.pointerCurrentRaycast.screenPosition - startPoint;
+
+            // Check if it should be recognised as Click.
+            float distance = deltaPoint.magnitude;
+            if (distance < clickThreshold) return;
+
+            // Otherwise calculate drag angle.
+            deltaPoint.Normalize();
+
+            float angle = (Mathf.Atan2(deltaPoint.x, deltaPoint.y) * Mathf.Rad2Deg + 360f) % 360f;
+
+            if (InputDerectionCallback != null)
+            {
+                angle += 45;
+                angle %= 360;
+                InputDerectionCallback((InputDirection)(angle / 90));
+            }
+        }
+    }
+
+    public enum InputDirection
+    {
+        Top,
+        Right,
+        Bottom,
+        Left
+    }
+}
+
---
+++ Assets/Sample/Input/SwipeInputDetector/SwipeInputTester.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using Quasar.Input;
+
+public class SwipeInputTester : MonoBehaviour
+{
+    [SerializeField] Text inputText;
+
+    private void Awake()
+    {
+        SwipeInputDetector.InputDerectionCallback += HandleSwipeInput;
+    }
+
+    void HandleSwipeInput(InputDirection direction)
+    {
+        inputText.text = direction.ToString();    
+    }
+}
