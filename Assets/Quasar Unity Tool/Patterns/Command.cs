ea2da47377df6fe826b18b5bc28072b34b4051e6
Terry K <terry@gixgames.com>
2017-08-23 13:46:20 +0900

    Implement command pattern
    Implement command pattern

---
+++ Assets/Quasar Unity Tool/Patterns/Command.cs
@@ -0,0 +1,21 @@
+﻿////////////////////////////////////////////////////////////////////////////
+// Command.cs - Applies Command pattern of GoF.
+//
+// Copyright (C) 2016 Terry K. @ GIX Entertainment Inc. & Quasar Inc.
+//
+////////////////////////////////////////////////////////////////////////////
+
+namespace Quasar.Patterns
+{
+    public abstract class Command
+    {
+        public abstract double TimeStamp
+        {
+            get;
+            set;
+        }
+
+        public abstract void Execute();
+        public abstract void Undo();
+    }
+}
